0c308a46e69a46696658988ca7ce721b00490643
PedroCR5 <123810586+PedroCR5@users.noreply.github.com>
2026-04-11 08:44:53 +0200

    PruebasMemory

--- src/games/memory/memory.js
+++ src/games/memory/memory.js
@@ -1,4 +1,5 @@
 //import { gameMemory } from "../../main";
+import { createCards } from "./Card";
 import { getImages } from "./infoAPI";
 
 export function pintarMemory() {
@@ -17,7 +18,7 @@ export function pintarMemory() {
   player2.id = "cursorText2";
   player2.innerText = "Jugador 2";
   let anteriorGanadorMemory = localStorage.getItem("ultimoGanadorMemory");// Recupero el último ganador para ponerlo debajo del tablero
-  console.log(anteriorGanadorMemory);
+  //console.log(anteriorGanadorMemory);
 
   let partidasGanadasMemoryJugador1 = localStorage.getItem("partidasGanadasMemoryJugador1") || 0;
   let partidasGanadasMemoryJugador2 = localStorage.getItem("partidasGanadasMemoryJugador2") || 0;
@@ -30,13 +31,42 @@ export function pintarMemory() {
   const memoryReset = document.querySelector(`#memoryReset`);;
   memoryReset.innerHTML = "";
   memoryReset.textContent = "Resetea la partida aquí";
+
+  //Pintar el input del tema del juego
+  const memoryTitle = document.getElementById("memoryTitle");
+  const inputMemory = document.createElement("input");
+  memoryTitle.append(inputMemory);
+  //inputMemory.className = "inputMe";
+  inputMemory.id = "inputMemory";
+  inputMemory.placeholder = "Escribe aquí el tema del juego";
+
+  const inputMemoryButton = document.createElement("button");
+  inputMemoryButton.id = "inputMemoryButton";
+  inputMemoryButton.textContent = "Pulsa para cambiarlo"
+  memoryTitle.append(inputMemoryButton);
+
+  inputMemoryButton.addEventListener('click', function () {
+    const temaMemory = document.getElementById("inputMemory").value;
+
+    changeImagesMemory(temaMemory);
+  });
+
+
 };
 
 export function gameMemory() {
   pintarMemory(); // Pinto el HTML
-  getImages('dog'); //Voy a infoAPI.js para recoger la info y pinto las Cards en Card.js.
-};
 
+  getImages('casa'); //Voy a infoAPI.js para recoger la info y pinto las Cards en Card.js.
+  //getImages('temaMemory'); //Voy a infoAPI.js para recoger la info y pinto las Cards en Card.js.
+
+};
+function changeImagesMemory(temaMemory) {
+  console.log(temaMemory);
+  //borrar las cards y ponerlas de nuevo
+  createCards(temaMemory);
+  gameMemory();
+}
 export function clickResetMemory() {// Función para resetear el memory
   const resetMemory = document.querySelector(`#memoryReset`);
   resetMemory.addEventListener('click', () => {
